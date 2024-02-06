fe03458196dfb9ff4a61a0a4832ddb711747efcd
Tushar Viradiya <tusharviradiya2@gmail.com>
2024-02-06 14:07:33 +0530

    variable understanding is clear

--- 01_basic/04_variable.js
+++ 01_basic/04_variable.js
@@ -5,6 +5,19 @@ const accountId = 123456;
 let accountEmail = "tusharviradiya2@gmail.com";
 var accountpassword = "123@321";
 accountCity = "Ahmedabad";
+let accountState;
 // accountId = 3; //we not assigning a new value to const variable.
 
 console.log(accountId);
+
+//table
+console.table([
+  accountId,
+  accountEmail,
+  accountpassword,
+  accountCity,
+  accountState,
+]);
+
+/*NOTE*/
+/*prefer not to use var coz block scope and function scope*/
