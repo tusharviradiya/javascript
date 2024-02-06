6bc075c7325356800fcd9d1f9b2e0fe3ce574dae
Tushar Viradiya <tusharviradiya2@gmail.com>
2024-02-06 16:31:33 +0530

    number and maths

---
+++ 01_basic/12_numberMaths.js
@@ -0,0 +1,15 @@
+//Number
+// let score = 400;
+// console.log(score);
+
+// const balance = Number(100);
+// console.log(balance);
+
+//Math library
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5654));
+// console.log(Math.ceil(5.5));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1);
