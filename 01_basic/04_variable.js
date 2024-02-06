19a9f393e89e59a9f183e19d4c1a2531c381972c
Tushar Viradiya <tusharviradiya2@gmail.com>
2024-02-06 13:41:14 +0530

    const behaviour

--- 01_basic/04_variable.js
+++ 01_basic/04_variable.js
@@ -5,4 +5,6 @@ const accountId = 123456;
 let accountEmail = "tusharviradiya2@gmail.com";
 var accountpassword = "123@321";
 accountCity = "Ahmedabad";
+// accountId = 3; //we not assigning a new value to const variable.
+
 console.log(accountId);
