5736d348f4296c19c10e17527279b7a413ebb521
Tushar Viradiya <tusharviradiya2@gmail.com>
2024-02-06 16:39:49 +0530

    date and time

---
+++ 01_basic/13_datetime.js
@@ -0,0 +1,9 @@
+//Dates
+
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toLocaleString());
