ec7f44ca0a056d17661de8e1e277cd3a37ff1f52
Tushar Viradiya <tusharviradiya2@gmail.com>
2024-02-06 14:39:00 +0530

    all about Data type

---
+++ 01_basic/05_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treats all js code as newer version
+
+// alert(3 + 3); we using nodeJs note browser
+
+/*data type*/
+
+//number: bigInt() for big Interger
+let a = 10;
+//string : ""
+let b = "tushar";
+
+//boolean : true, false.
+
+//null : standalone value
+
+//undefined : value is not assigned
+
+//symble : when we talk about unique.
+
+//typeof function
+console.log(typeof a);
+console.log(typeof null);
+console.log(typeof undefined);
+
+/*
+NOTE:
+code readability is more important.
+*/
