13b7daddede329c7ccce48280848f77fa1218658
Tushar Viradiya <tusharviradiya2@gmail.com>
2024-02-06 13:32:39 +0530

    basic about variable

---
+++ 01_basic/04_variable.js
@@ -0,0 +1,8 @@
+//we do invastigation wali study
+//
+
+const accountId = 123456;
+let accountEmail = "tusharviradiya2@gmail.com";
+var accountpassword = "123@321";
+accountCity = "Ahmedabad";
+console.log(accountId);
