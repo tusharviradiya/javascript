f0155f27d72037084ffdefdfb78a5eb55f7ed3e3
Tushar Viradiya <tusharviradiya2@gmail.com>
2024-02-08 11:59:07 +0530

    all about async function

---
+++ 04_async_function/script.js
@@ -0,0 +1,5 @@
+//javascript is synchronous, single threaded
+//blocking code and non blocking code ; ek work ko stop karke dusra kam hota he
+//js engine basically build by memory heap and call stack.
+//event loop : js engine -> web API -> task queue -> js engine
+//
